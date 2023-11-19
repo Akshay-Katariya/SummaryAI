a43bc02e5d63a7b2a931bb9a95bfe811e1a769a3
Akshay Katariya <akshay.katariya1@gmail.com>
2023-11-20 00:15:17 +0530

    Demo component UI

--- src/component/Demo.jsx
+++ src/component/Demo.jsx
@@ -1,5 +1,41 @@
+import { copy, linkIcon, loader, tick } from "../assets";
+
+const handleSubmit = () => {};
+
 const Demo = () => {
-  return <div>Demo</div>;
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      {/* Search */}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center "
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter a url"
+            value={""}
+            onChange={() => {}}
+            required
+            className="url_input peer"
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            ↵
+          </button>
+        </form>
+        {/* Browse URL History */}
+      </div>
+      {/* Display Results */}
+    </section>
+  );
 };
 
 export default Demo;
